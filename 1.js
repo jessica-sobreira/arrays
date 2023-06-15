6bc9d0ccbba284ac4ea71245e789a28ea9b1cafc
Jessica Sobreira <j.jecristina@gmail.com>
2023-06-14 23:55:16 -0300

    praticando-arrays

---
+++ 1.js
@@ -0,0 +1,12 @@
+// 1. Criar um array e percorrê-lo utilizando o loop for
+// a. Crie um array com o nome "nomes" e adicione 4 nomes de
+// pessoas que você conhece
+// b. Utilize o loop for para percorrer o array e exibir os nomes na
+// tela
+
+let nomes = ['Maria', 'João', 'José', 'Joana'];
+
+for (let index = 0; index < nomes.length; index++) {
+    let element = nomes[index];
+    console.log(element);
+}
